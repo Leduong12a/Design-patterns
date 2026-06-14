977a24f420e4a837e4663a6041c416cf6c0b6cc1
Phùng Văn Duy <phungvanduy23052005@gmail.com>
2026-06-14 10:09:50 +0700

    refactor: restructure job domain imports, remove index barrel file, and implement create job use case

--- backend/src/modules/client/domain/job/index.ts
+++
@@ -1,5 +0,0 @@
-export { JobEntity } from './entities/job.entity';
-export { JobType } from './job.types';
-export type { IJobSummary, IJobDetail, IJobProps } from './job.types';
-export { JobFactoryRegistry } from './factories/job-factory.registry';
-
